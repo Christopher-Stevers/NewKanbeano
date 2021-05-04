eb7104c987da022c515c849d4aa072c57e7f8b30
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-05-04 12:00:08 -0400

    removed github and twitter oauth options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,17 +10,13 @@ export default NextAuth({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
     }),
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    //Providers.GitHub({
+    //  clientId: process.env.GITHUB_ID,
+    //  clientSecret: process.env.GITHUB_SECRET,
+    //}),
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    Providers.Twitter({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
     }),
   ],
   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
