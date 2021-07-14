ad258bf01e97790a0d40675df18b0cac0c8e9460
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-07-14 08:30:58 -0400

    added credits

--- pages/board/[id].js
+++ pages/board/[id].js
@@ -10,38 +10,39 @@ import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/client'
 import Header from '../../components/header'
 import ColorPicker from '../../components/colorPicker'
-export default function Home () {
+export default function Home() {
   const [session, loading] = useSession()
   const router = useRouter()
   const { id } = router.query
-  const ogDate=new Date(parseInt(id)).toDateString();
+  const ogDate = new Date(parseInt(id)).toDateString();
   const current = Date.now();
   const newContext = useContext(NewContext);
   const [contextState, updateContextState] = useState(
     [
     ])
   const [auth, updateAuth] = useState(true)
-  const [h2, updateH2]=useState("")
-  const [placeCard, updatePlaceCard]=useState("")
+  const [h2, updateH2] = useState("")
+  const [placeCard, updatePlaceCard] = useState("")
   useEffect(async () => {
-    if (session){
-      
-    const url = "/" + "api/movies?listDate=" + router.query.id
-    const response = await fetch(url)
-    const responseObj = await response.json();
-    if (typeof responseObj[0] === 'string') {
-      updateAuth(false)
+    if (session) {
+
+      const url = "/" + "api/movies?listDate=" + router.query.id
+      const response = await fetch(url)
+      const responseObj = await response.json();
+      if (typeof responseObj[0] === 'string') {
+        updateAuth(false)
+      }
+      else {
+        updateContextState(responseObj.data)
+        updateH2(responseObj.listTitle)
+        updatePlaceCard(true)
+      }
     }
-    else {
-      updateContextState(responseObj.data)
-      updateH2(responseObj.listTitle)
-      updatePlaceCard(true)
-    }}
   }, [router.query.id, session]);
 
   // If no session exists, display access denied message
-  if (!session) { return <div> <Header  /> <span>This board either does not exist, or you do not own it.</span> </div> }
-  
+  if (!session) { return <div> <Header /> <span>This board either does not exist, or you do not own it.</span> </div> }
+
 
   const grabName = (e) => {
     updateName(e.target.value)
@@ -76,15 +77,15 @@ export default function Home () {
       updateContextState(nextContext)
       return;
     }
-    if(result.type==="parentList"){
-    
+    if (result.type === "parentList") {
 
-      const clonedContext=JSON.parse(JSON.stringify(contextState));
+
+      const clonedContext = JSON.parse(JSON.stringify(contextState));
       const [reorderedItem] = clonedContext.splice(result.source.index, 1);
       clonedContext.splice(result.destination.index, 0, reorderedItem);
-      updateContextState( clonedContext);
+      updateContextState(clonedContext);
 
-      
+
 
 
 
@@ -106,64 +107,73 @@ export default function Home () {
 
     const url = "/" + "api/movies?listDate=" + router.query.id
     const response = await fetch(url, JSON.parse(JSON.stringify(options)))
-if(response.status===200){}
+    if (response.status === 200) { }
+  }
+  const invite=()=>{
+    console.log("invite");
   }
   return (
 
     <>{auth ?
-    <div><Header />
-        <div className={styles.listHeader}><h2 className={styles.h2}>{h2}</h2><span className={styles.date}> Created <time>{ogDate}</time></span>
-        <button className={styles.save}onClick={postToAPI}>{<svg className={styles.saveIcon} version="1.1" x="0px" y="0px" width="100px"
-    height="100px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" >
- <g id="_x37_7_Essential_Icons">
-   <path id="Save" d="M82.4,24.3l-9.8-9.8c-0.4-0.4-0.9-0.6-1.4-0.6H19c-1.1,0-2,0.9-2,2v68c0,1.1,0.9,2,2,2h62c1.1,0,2-0.9,2-2V25.8
+      <div><Header />
+        <div className={styles.listHeader}>
+          <h2 className={styles.h2}>{h2}</h2>
+          <button onClick={invite}>Invite</button>
+          <span className={styles.date}> Created 
+            <time>{ogDate}</time>
+          </span>
+          <button className={styles.save} onClick={postToAPI}>
+            {<svg className={styles.saveIcon} version="1.1" x="0px" y="0px" width="100px"
+            height="100px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" >
+            <g id="_x37_7_Essential_Icons">
+              <path id="Save" d="M82.4,24.3l-9.8-9.8c-0.4-0.4-0.9-0.6-1.4-0.6H19c-1.1,0-2,0.9-2,2v68c0,1.1,0.9,2,2,2h62c1.1,0,2-0.9,2-2V25.8
      C83,25.2,82.8,24.7,82.4,24.3z M31,18h38v32H31V18z M79,82H21V18h6v34c0,1.1,0.9,2,2,2h42c1.1,0,2-0.9,2-2V20.6l6,6V82z M64.8,24.5
      v19c0,1.1-0.9,2-2,2c-1.1,0-2-0.9-2-2v-19c0-1.1,0.9-2,2-2C63.9,22.5,64.8,23.4,64.8,24.5z"/>
- </g>
- <g id="Guides">
- </g>
- <g id="Info">
-   <g id="BORDER">
-     <path fill="#0000FF" d="M1504-930V754H-280V-930H1504 M1512-938H-288V762h1800V-938L1512-938z"/>
-   </g>
- </g>
- </svg>}</button></div>
-      < main className={styles.main} >
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <NewContext.Provider value={[contextState, updateContextState]}>
+            </g>
+            <g id="Guides">
+            </g>
+            <g id="Info">
+              <g id="BORDER">
+                <path fill="#0000FF" d="M1504-930V754H-280V-930H1504 M1512-938H-288V762h1800V-938L1512-938z" />
+              </g>
+            </g>
+          </svg>}</button></div>
+        < main className={styles.main} >
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <NewContext.Provider value={[contextState, updateContextState]}>
 
-          <div className={styles.flexWrapper}>  <Droppable key={1} droppableId="kingOfTheDrops" direction="horizontal" type="parentList">
-              {(provided, snapshot) => (
-                <div className={styles.list}
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                >{contextState.map((elem, index) => {
-                  return <Draggable key={uuidv4()} draggableId={index.toString() + "topLevel"} index={index}>
-                    {(provided, snapshot) => (
-                      <div className={styles.listItem}
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      ><CardContainer i={index} /> </div>
-                    )}
-                  </Draggable>
-                })}
+              <div className={styles.flexWrapper}>  <Droppable key={1} droppableId="kingOfTheDrops" direction="horizontal" type="parentList">
+                {(provided, snapshot) => (
+                  <div className={styles.list}
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >{contextState.map((elem, index) => {
+                    return <Draggable key={uuidv4()} draggableId={index.toString() + "topLevel"} index={index}>
+                      {(provided, snapshot) => (
+                        <div className={styles.listItem}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        ><CardContainer i={index} /> </div>
+                      )}
+                    </Draggable>
+                  })}
 
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable><div>
-           {(placeCard)?<div className={styles.addList}><button className={styles.button} onClick={addList} >+</button></div>: null} 
-            </div></div>
-          </NewContext.Provider>
-        </DragDropContext>
-        
-      </main >
-      
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable><div>
+                  {(placeCard) ? <div className={styles.addList}><button className={styles.button} onClick={addList} >+</button></div> : null}
+                </div></div>
+            </NewContext.Provider>
+          </DragDropContext>
+
+        </main >
+
       </div> : <div>Access denied, this is not your board <Link href='/'>Home</Link></div>}
-    
-    
-    <ColorPicker />
+
+
+      <ColorPicker />
     </>
   )
 }
---
+++ pages/credits.js
@@ -0,0 +1,13 @@
+
+import Header from '../components/header'
+export default function Home () {
+    const linkStyle={
+
+        textDecoration: "underline"
+    }
+  return(<div>{}
+  <Header />
+  <h1>Credits</h1>
+<a style={linkStyle}href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+</div>)
+}
