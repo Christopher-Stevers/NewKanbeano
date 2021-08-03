dd5c7dabd56db01209be6782f02b4f3a2b6eb20e
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-08-03 06:51:12 -0400

    fixed copy on main page

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,8 @@ export default function Home() {
   <Header />
 
 <main className={styles.main}><h2 className={styles.h2}>Kanban <em className={styles.em}>simplified</em></h2>
-<p className={styles.like}>Kanban  doesn't need to be complicated. A Board. With Lists. That's what you need, and that's what Kanbeano gives you. It features a  Check out the <span className={styles.link}><Link href="/demo">demo</Link></span> or <button onClick={signIn} className={styles.link}>sign in</button> to create an account and build save your own boards.</p></main>
+<p className={styles.like}>Kanban  doesn't need to be complicated. A Board. With Lists. That's what you need, and that's what Kanbeano gives you. Check out the 
+<span className={styles.link}><Link href="/demo">demo</Link></span> or <button onClick={signIn} className={styles.link}>sign in</button> to create an account and build save your own boards.</p></main>
 <ColorPicker/>
   </>
   )
